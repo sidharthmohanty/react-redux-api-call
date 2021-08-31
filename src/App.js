d566113612da6d3de84a0bc4cfe173a68edf966c
sidharthmohanty <sidharthmohanty21@gmail.com>
2021-08-31 22:26:52 +0530

    structured layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import React, { useEffect, useState } from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Home from "./components/Home";
+import TopNavbar from "./components/layouts/Navbar";
+
 function App() {
-  return <></>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const authToken = window.localStorage.getItem("authToken");
+    if (authToken) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  return (
+    <>
+      <Router>
+        <TopNavbar />
+        {isLoggedIn ? (
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        ) : (
+          <Switch>
+            <Route exact path="/home" component={Home} />
+          </Switch>
+        )}
+      </Router>
+    </>
+  );
 }
 
 export default App;
